5fd9b9b28fa582f2a5b0df0463ffff3f44329fe2
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-06-17 16:15:56 +0300

    Add stumps according to stump array

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
   window.onload = function() {
 
-    var game = new Phaser.Game(1200, 900, Phaser.CANVAS, 'game-container');
+    var game = new Phaser.Game(1200, 800, Phaser.CANVAS, 'game-container');
     var cursors;
     var playGame = function(game){}
     var step = 220;
-
+    var stumpSizeX = 102;
+    var stumpSizeY = 52;  
+    var stumpSpaceX = 14;
+    var stumpSpaceY = 5;
+    var myRaccoonStumpStartX = 150;
+    var myRaccoonStumpStartY = 480;
+   
     playGame.prototype = {
 
          preload: function() {
             game.load.spritesheet('landscape', '/assets/landscape.jpg');
-            game.load.spritesheet('raccoon_side', '/assets/raccoon_right.png') 
+            game.load.spritesheet('raccoon_side', '/assets/raccoon_side.png', 159, 153) 
+            game.load.spritesheet('stump', '/assets/stump.png');
 //            game.load.atlasJSONHash('bot', '/assets/running_bot.png', '/assets/running_bot.json');
 //            game.load.spritesheet('mummy', '/assets/metalslug_mummy37x45.png', 37, 45, 18);
         },
@@ -18,13 +25,13 @@
 
          create: function() {
             var stumpsArray = this.arrageStumps();
-            game.world.bounds = new Phaser.Rectangle(100, 50, 1000, 800); 
+            game.world.bounds = new Phaser.Rectangle(100, 50, 1000, 700); 
             game.physics.startSystem(Phaser.Physics.ARCADE);
             game.physics.setBoundsToWorld();
 
             game.add.sprite(0, 0, 'landscape'); 
-             
-            this.raccoon = game.add.sprite(350, 730, 'raccoon_side');
+            this.drawStumps(stumpsArray); 
+            this.raccoon = game.add.sprite(350, 650, 'raccoon_side', 0);
             this.raccoon.anchor.setTo(0.5, 0.5);
             this.physics.arcade.enable(this.raccoon);
             this.raccoon.body.collideWorldBounds = true;
@@ -50,14 +57,14 @@
             if (cursors.left.isDown)
             {
                 //  Move to the left
-                this.raccoon.scale.x = -1;
+                this.raccoon.loadTexture('raccoon_side', 0);
                 this.raccoon.body.velocity.x = -step;
 //                this.raccoon.animations.play('run');
             }
              else if (cursors.right.isDown)
             {
                 //  Move to the right
-                this.raccoon.scale.x = 1;
+                this.raccoon.loadTexture('raccoon_side', 1);
                 this.raccoon.body.velocity.x = step;
 //                this.raccoon.animations.play('run');
             }
@@ -89,8 +96,19 @@
                 stumpsArray[j] = temp;
             }
             return stumpsArray;
+        },
+        
+        drawStumps: function(stumpsArray){
+            for (var i=0; i < stumpsArray.length; i++){
+                for (var j=0; j < 3; j++ ){
+                    if (stumpsArray[i][j] == 1){
+                    game.add.image(myRaccoonStumpStartX + i * (stumpSizeX + stumpSpaceX), myRaccoonStumpStartY + j * (stumpSizeY + stumpSpaceY), "stump");
+                
+                }
+            }
         }
-
+        
+        
 //function render() {
 //
 //    game.debug.body(bot);
