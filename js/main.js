374dbf46ad8cfbe0014bbf0cefd48f4ef5b2ab33
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-06-18 01:46:56 +0300

    Add jumpint raccoon, and clothes

--- js/main.js
+++ js/main.js
@@ -3,72 +3,120 @@
     var game = new Phaser.Game(1200, 800, Phaser.CANVAS, 'game-container');
     var cursors;
     var playGame = function(game){}
-    var step = 220;
+
     var stumpSizeX = 102;
     var stumpSizeY = 52;  
     var stumpSpaceX = 14;
-    var stumpSpaceY = 5;
+    var stumpSpaceY = 25;      
     var myRaccoonStumpStartX = 150;
-    var myRaccoonStumpStartY = 480;
+    var myRaccoonStumpStartY = 440;
+      
+      
+    var raccoonStartX = 100;
+    var raccoonStartY = 330; 
+    var raccoonStepX = 116;  
+    var raccoonStepY = 77;
     var stumpIndent = [0, 8, 16, 24, 24, 16, 8, 0]  
     
+    var clothesGroup
+
    
     playGame.prototype = {
 
          preload: function() {
             game.load.spritesheet('landscape', '/assets/landscape.jpg');
-            game.load.spritesheet('raccoon_side', '/assets/raccoon_side.png', 159, 153) 
+            game.load.spritesheet('raccoon_side', '/assets/raccoon_side.png', 510, 509) 
+            game.load.spritesheet('raccoon_front', '/assets/raccoon_front.png', 465, 511) 
+            game.load.spritesheet('clothes', '/assets/clothes.png', 226, 212);
+
             game.load.spritesheet('stump', '/assets/stump.png');
-//            game.load.atlasJSONHash('bot', '/assets/running_bot.png', '/assets/running_bot.json');
+             
+            this.isUp = true;this.isDown = true;this.isLeft = true;this.isRight = true;
+//           game.load.atlasJSONHash('bot', '/assets/running_bot.png', '/assets/running_bot.json');
 //            game.load.spritesheet('mummy', '/assets/metalslug_mummy37x45.png', 37, 45, 18);
         },
 
 
 
          create: function() {
-            var stumpsArray = this.arrageStumps();
-            game.world.bounds = new Phaser.Rectangle(100, 50, 1000, 700); 
+            this.stumpsArray = this.arrageStumps();
+            game.world.bounds = new Phaser.Rectangle(50, 50, 1100, 700); 
             game.physics.startSystem(Phaser.Physics.ARCADE);
             game.physics.setBoundsToWorld();
-
+            
+          
             game.add.sprite(0, 0, 'landscape'); 
-            this.drawStumps(stumpsArray); 
-            this.raccoon = game.add.sprite(350, 650, 'raccoon_side', 0);
-            this.raccoon.anchor.setTo(0.5, 0.5);
+            this.clothesGroup = game.add.physicsGroup(); 
+            this.drawStumps(this.stumpsArray); 
+            this.raccoon = game.add.sprite(raccoonStartX, raccoonStartY+3*raccoonStepY, 'raccoon_side', 0);
+            this.raccoon.state = 'right' 
             this.physics.arcade.enable(this.raccoon);
             this.raccoon.body.collideWorldBounds = true;
-             
+            this.raccoon.scale.x = 0.3; 
+            this.raccoon.scale.y = 0.3; 
+            this.raccoon.positionX = 0;
+            this.raccoon.positionY = 3; 
+
             
 //            this.bot.animations.add('run');
 //            this.bot.animations.play('run', 15, true);
 
 //            this.input.onDown.addOnce(this.changeMummy, this);
-            cursors = game.input.keyboard.createCursorKeys();
+             cursors = game.input.keyboard.createCursorKeys();
 
         },
 
-//        changeMummy: function() {
-//            this.bot.loadTexture('mummy', 0);
-//            this.bot.animations.add('walk');
-//            this.bot.animations.play('walk', 30, true);
-//        },
-
-
         update: function(){
-            this.raccoon.body.velocity.x = 0
-            if (cursors.left.isDown)
+            if (cursors.left.isDown && this.isLeft)
             {
-                //  Move to the left
-                this.raccoon.loadTexture('raccoon_side', 0);
-                this.raccoon.body.velocity.x = -step;
-//                this.raccoon.animations.play('run');
+                if (this.raccoon.state != 'left'){
+                    this.raccoon.state = 'left';
+                    this.raccoon.loadTexture('raccoon_side', 1);
+                }
+                else {
+                    this.raccoon.positionX -= this.canGoToDirection('left');
+                }
+                this.drawRaccoon();
+                this.isLeft = false; 
             }
-             else if (cursors.right.isDown)
+             else if (cursors.right.isDown && this.isRight)
             {
-                //  Move to the right
-                this.raccoon.loadTexture('raccoon_side', 1);
-                this.raccoon.body.velocity.x = step;
-//                this.raccoon.animations.play('run');
+                if (this.raccoon.state != 'right'){
+                    this.raccoon.loadTexture('raccoon_side', 0);
+                    this.raccoon.state = 'right';
+                }    
+                else {
+                    this.raccoon.positionX += this.canGoToDirection('right');
+                }
+                this.drawRaccoon();
+                this.isRight = false; 
+            }
+            
+            else if (cursors.up.isDown && this.isUp)
+            {
+              if (this.raccoon.state != 'up'){
+                  this.raccoon.loadTexture('raccoon_front', 1);
+                  this.raccoon.state = 'up';
+              }
+              else {
+                  this.raccoon.positionY -= this.canGoToDirection('up');
+              }
+              this.drawRaccoon();    
+              this.isUp = false;        
+            }
+            
+            else if (cursors.down.isDown && this.isDown)
+            {
+              this.goFiber();
+              if (this.raccoon.state != 'down'){
+                  this.raccoon.loadTexture('raccoon_front', 0);
+                  this.raccoon.state = 'down';
+              }
+              else {
+                  this.raccoon.positionY += this.canGoToDirection('down');
+              }
+              this.drawRaccoon();
+              this.isDown = false;         
             }
 //            else
 //            {
@@ -77,19 +125,24 @@
 //
 //                this.bot.frame = 4;
 //            }
+            
+            if (cursors.up.isUp){this.isUp = true;}
+            if (cursors.down.isUp){this.isDown = true;}
+            if (cursors.left.isUp){this.isLeft = true;}
+            if (cursors.right.isUp){this.isRight = true;}
 
         },
 
         arrageStumps: function() {
             var stumpsArray = [
-                [1, 0, 0],
-                [0, 0, 1],
-                [0, 1, 0],
-                [0, 0, 1],
-                [0, 1, 1],
-                [1, 1, 0],
-                [1, 0, 1],
-                [1, 1, 1]
+                [1, 0, 0, 1],
+                [0, 0, 1, 1],
+                [0, 1, 0, 1],
+                [0, 0, 1, 1],
+                [0, 1, 1, 1],
+                [1, 1, 0, 1],
+                [1, 0, 1, 1],
+                [1, 1, 1, 1]
             ];
             for (i = 0; i < stumpsArray.length; i++) {
                 j = Math.floor(Math.random() * (i + 1));
@@ -105,11 +158,66 @@
                 for (var j=0; j < 3; j++ ){
                     if (stumpsArray[i][j] == 1){
                     game.add.image(myRaccoonStumpStartX + i * (stumpSizeX + stumpSpaceX), myRaccoonStumpStartY + j * (stumpSizeY + stumpSpaceY) + stumpIndent[i], "stump");
-                
+                    }
                 }
             }
-        }
-        }
+        },
+        
+        canGoToDirection: function(direction){
+          switch(direction) {
+              case 'up':
+                  if (this.stumpsArray[this.raccoon.positionX][this.raccoon.positionY-1] == 1) 
+                    {return 1}
+                  else if (this.stumpsArray[this.raccoon.positionX][this.raccoon.positionY-2] == 1)
+                    {return 2}
+                  else {return 0}   
+                  break;
+              case 'down':
+                  if (this.stumpsArray[this.raccoon.positionX][this.raccoon.positionY+1] == 1) 
+                    {return 1}
+                  else if (this.stumpsArray[this.raccoon.positionX][this.raccoon.positionY+2] == 1)
+                    {return 2}
+                  else {return 0}   
+                  break;
+              case 'right':
+                  if (this.stumpsArray[this.raccoon.positionX+1] && this.stumpsArray[this.raccoon.positionX+1][this.raccoon.positionY] == 1) 
+                    {return 1}
+                  else if (this.stumpsArray[this.raccoon.positionX+2] && this.stumpsArray[this.raccoon.positionX+2][this.raccoon.positionY] == 1)
+                    {return 2}
+                  else {return 0} 
+                  break;
+              case 'left':
+                  if (this.stumpsArray[this.raccoon.positionX-1] && this.stumpsArray[this.raccoon.positionX-1][this.raccoon.positionY] == 1) 
+                    {return 1}
+                  else if (this.stumpsArray[this.raccoon.positionX-2] && this.stumpsArray[this.raccoon.positionX-2][this.raccoon.positionY] == 1)
+                    {return 2}
+                  else {return 0} 
+                  break;
+          }
+            
+        },
+        
+        goFiber: function(){
+            var cloth = this.clothesGroup.create(-73, 560, 'clothes', 5); 
+            console.log(cloth);
+            this.physics.arcade.enable(cloth);
+            cloth.scale.x = 0.3; 
+            cloth.scale.y = 0.3; 
+            cloth.body.velocity.x = 100;
+//            clothesGroup.add(cloth);
+
+            
+        },
+        
+        drawRaccoon: function(){
+            var leftCorrect = this.raccoon.state == 'left' ? 70 : 0;  
+            var upCorrect = this.raccoon.state == 'up'  || this.raccoon.state == 'down' ? 55 : 0;
+            this.raccoon.body.x = raccoonStartX + this.raccoon.positionX * raccoonStepX + leftCorrect + upCorrect;
+            console.log(this.raccoon.state);
+            console.log(raccoonStartY + this.raccoon.positionY * raccoonStepY);
+            this.raccoon.body.y = raccoonStartY + this.raccoon.positionY * raccoonStepY;
+        },
+        
         
 //function render() {
 //
